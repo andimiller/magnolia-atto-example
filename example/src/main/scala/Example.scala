91c00c8c6ba3cce5dd31adb5a9cc8bb682d47c0f
Andi Miller <andi@andimiller.net>
2020-02-05 23:53:51 +0000

    add example for trait

--- example/src/main/scala/Example.scala
+++ example/src/main/scala/Example.scala
@@ -9,6 +9,20 @@ object Foo {
   implicit val fooParser: Parser[Foo] = AttoDeriver.gen[Foo]
 }
 
+sealed trait Letter
+case object A extends Letter
+case object B extends Letter
+case object C extends Letter
+object Letter {
+  implicit val aParser: Parser[A.type] = char('a').as(A)
+  implicit val bParser: Parser[B.type] = char('b').as(B)
+  implicit val cParser: Parser[C.type] = char('c').as(C)
+  implicit val letterParser: Parser[Letter] = AttoDeriver.gen[Letter]
+}
+
 object Example extends App {
   println(Foo.fooParser.parseOnly("123abc").either)
+  "abc".foreach { l =>
+    println(Letter.letterParser.parseOnly(l.toString))
+  }
 }
